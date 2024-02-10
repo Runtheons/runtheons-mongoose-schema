2479e556d834a220e35c16582e3130b96c9ae2b5
Zexal0807 <gallinar00@gmail.com>
2024-02-10 08:04:54 +0100

    Implment more tests

--- tests/Athlete.test.js
+++ tests/Athlete.test.js
@@ -1,13 +1,55 @@
-const { setUp } = require("./db");
+const { setUp, dropDatabase, dropCollections } = require("./db");
 
 describe("ATHLETE", () => {
 	let database = undefined;
 
-	beforeEach(async() => {
+	beforeAll(async() => {
 		database = await setUp()
 	});
 
-	afterEach(async() => {});
+	afterAll(async() => {
+		await dropDatabase();
+	});
+
+	afterEach(async() => {
+		await dropCollections();
+	});
+
+	test("C - Create an athlete", async() => {
+		const { Athlete } = database;
+
+		let a = await Athlete.find();
+		expect(a.length).toEqual(0);
+
+		let at = new Athlete({ name: "Roberto", surname: "Gallina" });
+		await at.save();
+
+		let b = await Athlete.find();
+		expect(b.length).toEqual(1);
+	});
+
+
+	test("R - Read all sex", async() => {
+		const { Athlete } = database;
+
+		let a = await Athlete.find();
+
+		expect(a.length).toEqual(0);
+	});
+
+	test("C - Create an athlete", async() => {
+		const { Athlete } = database;
+
+		let a = await Athlete.find();
+		expect(a.length).toEqual(0);
+
+		let at = new Athlete({ name: "Roberto", surname: "Gallina" });
+		await at.save();
+
+		let b = await Athlete.find();
+		expect(b.length).toEqual(1);
+	});
+
 
 	test("R - Read all sex", async() => {
 		const { Athlete } = database;
