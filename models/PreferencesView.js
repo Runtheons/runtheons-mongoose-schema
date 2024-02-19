fd21dff5da0293551d7849764c210abfa8885ed6
Zexal0807 <gallinar00@gmail.com>
2024-02-19 22:24:30 +0100

    Fix name

--- models/PreferencesView.js
+++ models/PreferencesView.js
@@ -1,13 +1,13 @@
 module.exports = (mongoose) => {
 	const { Schema } = mongoose;
 
-	const PreferencesView = new Schema({
+	const PreferencesViewSchema = new Schema({
 		athlete: Schema.Types.ObjectId,
 		performanceExpert: Schema.Types.ObjectId,
 		preferences: {}
 	}, { versionKey: false });
 
-	const model = mongoose.model('PreferencesView', PreferencesView, 'preferencesView');
+	const model = mongoose.model('PreferencesView', PreferencesViewSchema, 'preferencesView');
 
 	return model;
 }
