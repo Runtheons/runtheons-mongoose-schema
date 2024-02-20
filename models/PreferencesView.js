355714f8734453e1b1ca514cc001cc12914e1675
Zexal0807 <gallinar00@gmail.com>
2024-02-20 08:19:59 +0100

    Rename var

--- models/PreferencesView.js
+++ models/PreferencesView.js
@@ -1,14 +1,14 @@
 module.exports = (mongoose) => {
 	const { Schema } = mongoose;
 
-	const PreferencesSchema = new Schema({
+	const IncapsultedPreferencesSchema = new Schema({
 
 	});
 
 	const PreferencesViewSchema = new Schema({
 		athlete: Schema.Types.ObjectId,
 		performanceExpert: Schema.Types.ObjectId,
-		preferences: PreferencesSchema
+		preferences: IncapsultedPreferencesSchema
 	}, { versionKey: false });
 
 	const model = mongoose.model('PreferencesView', PreferencesViewSchema, 'preferencesView');
