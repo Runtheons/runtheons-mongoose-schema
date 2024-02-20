8f1f95ac145404fad3a694e4068fc34b3ee5060f
Zexal0807 <gallinar00@gmail.com>
2024-02-20 08:06:10 +0100

    Add empty preferences

--- models/PreferencesView.js
+++ models/PreferencesView.js
@@ -1,10 +1,14 @@
 module.exports = (mongoose) => {
 	const { Schema } = mongoose;
 
+	const PreferencesSchema = new Schema({
+
+	});
+
 	const PreferencesViewSchema = new Schema({
 		athlete: Schema.Types.ObjectId,
 		performanceExpert: Schema.Types.ObjectId,
-		preferences: {}
+		preferences: PreferencesSchema
 	}, { versionKey: false });
 
 	const model = mongoose.model('PreferencesView', PreferencesViewSchema, 'preferencesView');
