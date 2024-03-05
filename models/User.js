740a241b6933ac8cf67650921e1886ec5b4e8b1a
Zexal0807 <gallinar00@gmail.com>
2024-03-05 11:44:38 +0100

    Add default view

--- models/User.js
+++ models/User.js
@@ -28,7 +28,42 @@ const UserSchema = new Schema({
 	},
 }, { versionKey: false });
 
-
 const model = mongoose.model('User', UserSchema, 'users');
+
+
+const f = model.find;
+model.find = (filter = {}, projection, options) => f.apply(model, [
+	filter,
+	{...projection, performanceExperts: 0, athletes: 0, status: 0 },
+	options
+]);
+
+const fo = model.findOne;
+model.findOne = (filter = {}, projection, options) => fo.apply(model, [
+	filter,
+	{...projection, performanceExperts: 0, athletes: 0, status: 0 },
+	options
+]);
+
+const fod = model.findOneAndDelete;
+model.findOneAndDelete = (filter = {}, options) => fod.apply(model, [
+	filter,
+	options
+]);
+
+const fore = model.findOneAndReplace;
+model.findOneAndReplace = (filter = {}, replacement, options) => fore.apply(model, [
+	filter,
+	replacement,
+	options
+]);
+
+const fou = model.findOneAndUpdate;
+model.findOneAndUpdate = (filter = {}, update, options) => fou.apply(model, [
+	{...filter, type: "ATHLETE" },
+	update,
+	options
+]);
+
 
 module.exports = model;
