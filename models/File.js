7993e17d1a8e19319833058f74ae55a4d3cd1da8
Zexal0807 <gallinar00@gmail.com>
2024-03-26 20:57:56 +0100

    Add filed

--- models/File.js
+++ models/File.js
@@ -13,6 +13,10 @@ const FileSchema = new Schema({
 		type: String,
 		required: true
 	},
+	upload: {
+		type: Date,
+		default: Date.now
+	},
 	creator: {
 		type: new Schema({
 			name: {
@@ -32,62 +36,6 @@ const FileSchema = new Schema({
 		enum: Enum.FILE_TAGS
 	},
 }, { versionKey: false });
-
-// AthleteSchema.methods.hasPerformanceExpert = function(_id) {
-// 	return this.performanceExperts.filter(x => x._id.equals(_id)).length > 0 || null;
-// }
-
-// AthleteSchema.methods.addPerformanceExpert = async function(performanceExpert) {
-// 	if (this.hasPerformanceExpert(performanceExpert._id))
-// 		return false
-// 	let { _id, name, surname, photo, title } = performanceExpert
-// 	this.performanceExperts.push({ _id, name, surname, photo, title });
-// 	await this.save();
-// }
-
-// AthleteSchema.methods.removePerformanceExpert = async function(_id) {
-// 	if (!this.hasPerformanceExpert(_id))
-// 		return false
-// 	this.performanceExperts.pull({ _id });
-// 	await this.save();
-// }
-
-// AthleteSchema.methods.setName = async function(newName) {
-// 	this.name = newName;
-// 	await this.save();
-
-// 	const { PerformanceExpert } = mongoose.models;
-
-// 	await PerformanceExpert.updateMany({ 'athletes._id': this._id }, {
-// 		'$set': {
-// 			'athletes.$.name': newName
-// 		}
-// 	});
-// }
-// AthleteSchema.methods.setSurname = async function(newSurname) {
-// 	this.surname = newSurname;
-// 	await this.save();
-
-// 	const { PerformanceExpert } = mongoose.models;
-
-// 	await PerformanceExpert.updateMany({ 'athletes._id': this._id }, {
-// 		'$set': {
-// 			'athletes.$.surname': newSurname
-// 		}
-// 	});
-// }
-// AthleteSchema.methods.setPhoto = async function(newPhoto) {
-// 	this.photo = newPhoto;
-// 	await this.save();
-
-// 	const { PerformanceExpert } = mongoose.models;
-
-// 	await PerformanceExpert.updateMany({ 'athletes._id': this._id }, {
-// 		'$set': {
-// 			'athletes.$.photo': newPhoto
-// 		}
-// 	});
-// }
 
 const model = mongoose.model('File', FileSchema, 'files');
 
